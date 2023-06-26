1faf715e7cf1d49a4475b07088d97c92cecabc11
TheSimorgh <TheSimurgh30@proton.me>
2023-06-26 19:56:07 +0500

    + cancel_request route + ctrl

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -312,14 +312,45 @@ exports.activateAccount = async (req, res) => {
           .json({ message: "You can't send a request to yourself" });
       }
     } catch (error) {
-      
+      res.status(500).json({ message: error.message });
     }
   }
+  exports.cancelRequest = async (req, res) => {
+    try {
+      if (req.user.id !== req.params.id) {
+        const sender = await User.findById(req.user.id);
+        const receiver = await User.findById(req.params.id);
+        if (
+          receiver.requests.includes(sender._id) &&
+          !receiver.friends.includes(sender._id)
+        ) {
+          await receiver.updateOne({
+            $pull: { requests: sender._id },
+          });
+          await receiver.updateOne({
+            $pull: { followers: sender._id },
+          });
+          await sender.updateOne({
+            $pull: { following: sender._id },
+          });
+          res.json({ message: "you successfully canceled request" });
+        } else {
+          return res.status(400).json({ message: "Already Canceled" });
+        }
+      } else {
+        return res
+          .status(400)
+          .json({ message: "You can't cancel a request to yourself" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
   exports.auth=async (req,res)=>{
     try {
       console.log(`auth`);
       console.log(req.user);
     } catch (error) {
-      
+      res.status(500).json({ message: error.message });
     }
   }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register,activateAccount, login,auth, sendVerification, findUser, sendResetPasswordCode, changePassword, getProfile,updateProfilePicture, updateCover, updateDetails, addFriend } = require("../controllers/user");
+const { register,activateAccount, login,auth, sendVerification, findUser, sendResetPasswordCode, changePassword, getProfile,updateProfilePicture, updateCover, updateDetails, addFriend, cancelRequest } = require("../controllers/user");
 const { authMidd } = require("../middlwares/authMidd");
 
 
@@ -17,6 +17,7 @@ router.put("/updateProfilePicture", authMidd,updateProfilePicture);
 router.put("/updateCover", authMidd,updateCover);
 router.put("/updateDetails", authMidd,updateDetails);
 router.put("/addFriend/:id", authMidd,addFriend);
+router.put("/cancelRequest/:id", authMidd,cancelRequest);
 
 
 getProfile
