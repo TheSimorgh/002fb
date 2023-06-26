a0d43d778d7a9c41044dea508586d4b9d8696047
TheSimorgh <TheSimurgh30@proton.me>
2023-06-26 20:58:51 +0500

    + follow + unfollow routes + ctrls

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register,activateAccount, login,auth, sendVerification, findUser, sendResetPasswordCode, changePassword, getProfile,updateProfilePicture, updateCover, updateDetails, addFriend, cancelRequest } = require("../controllers/user");
+const { register,activateAccount, login,auth, sendVerification, findUser, sendResetPasswordCode, changePassword, getProfile,updateProfilePicture, updateCover, updateDetails, addFriend, cancelRequest, follow, unFollow } = require("../controllers/user");
 const { authMidd } = require("../middlwares/authMidd");
 
 
@@ -18,7 +18,8 @@ router.put("/updateCover", authMidd,updateCover);
 router.put("/updateDetails", authMidd,updateDetails);
 router.put("/addFriend/:id", authMidd,addFriend);
 router.put("/cancelRequest/:id", authMidd,cancelRequest);
-
+router.put("/follow/:id", authMidd,follow);
+router.put("/follow/:id", authMidd,unFollow);
 
 getProfile
 router.post("/auth",authMidd, auth)
